179b64a579a617e15127b1adcdc7a16363a92f23
takuyawww <wakataku.11010809@gmail.com>
2023-05-06 18:43:18 +0900

    change api route

--- app-1/main.go
+++ app-1/main.go
@@ -4,10 +4,10 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	r.GET("/api/version", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"name":    "app-1",
-			"version": "0.0.2",
+			"version": "0.0.3",
 		})
 	})
 	r.Run(":8081")
